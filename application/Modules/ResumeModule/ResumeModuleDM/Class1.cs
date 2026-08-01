2079f08e50670976f5bdbbeee4233d1afd08542a
Singh Anup <anup.singh@gruppomol.it>
2026-08-01 12:12:18 +0530

    resume requirment added

--- application/Modules/ResumeModule/ResumeModuleDM/Class1.cs
+++ application/Modules/ResumeModule/ResumeModuleDM/Class1.cs
@@ -1,7 +1,24 @@
-﻿namespace ResumeModuleDM
-{
-    public class Class1
-    {
-
-    }
-}
+﻿/*
+ Entities to store the resume data for the user
+1. Resume: Title, Summary, Photo, ResumeTemplate, UserId - FK
+2. PeronalInformation
+    Address: - id, City, State, Country, ZipCode, street address - FK
+    Email , useSameAsProfile
+    PhoneNumber, useSameAsProfile
+    Awards, useSameAsProfile - FK
+    Languages, useSameAsProfile - FK
+    hobbies, useSameAsProfile - Fk    
+    socialMediaLinks, useSameAsProfile - FK
+3. Education
+    PassingYear, Degree, Institution, City, State, Description, percentage, Grade, isCurrent
+r. Certifications
+    CertificationName, IssuingOrganization, IssueDate, ExpirationDate, CredentialID, CredentialURL, Description
+5. Skills
+    SkillName, ProficiencyLevel, YearsOfExperience, Description
+6. WorkExperience
+    JobTitle, CompanyName, StartDate, EndDate, Location, Description, isCurrent
+7. Projects
+    ProjectName, Role, StartDate, EndDate, Description, TechnologiesUsed, isCurrent
+4. Setup Table: List of Education, Certifications, Skills, Languages, WorkExperience, Projects, Awards, Hobbies, SocialMediaLinks
+  
+ */
