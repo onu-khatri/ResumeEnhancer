78ff6338ffe48e62d1433f6803cebc326f696709
Singh Anup <anup.singh@gruppomol.it>
2026-08-06 11:17:52 +0530

    add project paths to solution

--- application/Infrastructure/Caching/Class1.cs
+++ application/Infrastructure/Caching/Class1.cs
@@ -1,7 +1,6 @@
-﻿namespace Caching
+﻿// Implement service for in-memory caching of data. This service will provide methods to store, retrieve, and manage cached data efficiently.
+// Use .net core inmomory
+namespace Caching
 {
-    public class Class1
-    {
-
-    }
+    
 }
